a0306f6cd6a2a2763b5508d2f24d0de8a3eef8a5
Rajat-Rathaur <142532314+Rajat-Rathaur@users.noreply.github.com>
2024-06-16 05:27:08 +0530

    fix

--- frontend/src/pages/Navbar.jsx
+++ frontend/src/pages/Navbar.jsx
@@ -134,18 +134,7 @@ export default function Navbar() {
                     leaveTo="transform opacity-0 scale-95"
                   >
                     <MenuItems className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <MenuItem>
-                        {({ focus }) => (
-                   
-                          <a
-                          href="/profile"
-                         
-                          className={classNames(focus ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                        >
-                          Your Profile
-                        </a>
-                        )}
-                      </MenuItem>
+                  
                  
                       <MenuItem>
                         {({ focus }) => (
